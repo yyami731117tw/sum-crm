2cc24110ed96296ef0a70af6df6e59f664984e6a
yyami731117tw <yyami731117tw@yahoo.com.tw>
2024-12-31 14:42:30 +0800

    v0.1.1

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  pageExtensions: ['tsx', 'ts'],
-  env: {
-    NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false };
-    return config;
-  },
-  output: 'standalone'
 }
 
 module.exports = nextConfig 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import '../src/styles/globals.css'
+import '../styles/globals.css'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="B2B CRM Dashboard" />
+        <title>B2B CRM</title>
       </Head>
       <Component {...pageProps} />
     </>
   )
-} 
+}
+
+export default MyApp 
