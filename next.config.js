bb84f79ac71c56380a218bec293f48cac0f094c0
yyami731117tw <yyami731117tw@yahoo.com.tw>
2025-01-04 17:35:20 +0800

    fix: update path aliases and move utils to src directory

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['localhost']
-  }
+  experimental: {
+    serverActions: true,
+  },
 }
 
 module.exports = nextConfig 
