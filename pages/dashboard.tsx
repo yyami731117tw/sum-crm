0ac71c43dab9514076155c6cc7a0dcf3e945a1c1
yyami731117tw <yyami731117tw@yahoo.com.tw>
2025-01-04 16:51:50 +0800

    feat: redirect dashboard to home page

---
+++ pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+export default function Dashboard() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/')
+  }, [router])
+
+  return null
+} 
