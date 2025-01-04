670fd6ae7144e30490a3f751d5a2624c3f093324
yyami731117tw <yyami731117tw@yahoo.com.tw>
2025-01-04 17:30:40 +0800

    chore: update eslint rules and fix prisma types

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
+const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined }
+
+if (!globalForPrisma.prisma) {
+  globalForPrisma.prisma = new PrismaClient()
 }
 
-const prisma = global.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma
-}
+const prisma = globalForPrisma.prisma as PrismaClient
 
 export default prisma 
