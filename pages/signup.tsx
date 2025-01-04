0b4b749038c272cc2570a8b7a66c2de2ab62a920
yyami731117tw <yyami731117tw@yahoo.com.tw>
2025-01-04 18:03:11 +0800

    fix: update password strength indicator import path

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import PasswordStrengthIndicator from '../components/PasswordStrengthIndicator'
+import PasswordStrengthIndicator from '@/components/PasswordStrengthIndicator'
 import { checkPasswordStrength } from '../utils/password'
 
 const Signup: NextPage = () => {
