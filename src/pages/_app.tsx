9dcdec1fd41980ce44ca6364d44553dcf8792a74
yyami731117tw <yyami731117tw@yahoo.com.tw>
2025-01-10 17:58:41 +0800

    修復: 更新 SessionProvider 配置

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,11 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider 
+      session={session}
+      refetchInterval={0}
+      refetchOnWindowFocus={false}
+    >
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>B2B CRM</title>
